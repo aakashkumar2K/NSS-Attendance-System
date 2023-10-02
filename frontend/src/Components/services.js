fcc15c056e29cc8243d8a717cd53d854e69be2d5
AmanAgrahariaa <agraharia329@gmail.com>
2023-10-02 09:57:06 +0530

    launched

--- frontend/src/Components/services.js
+++ frontend/src/Components/services.js
@@ -1,3 +1,3 @@
 // export const backend_url = "http://localhost:5000";
-
-export const backend_url = 'https://nssattendancebackend.onrender.com/';
+// https://nssattendancebackend.onrender.com
+export const backend_url = 'https://nssattendancebackend.onrender.com';
