51309736c8caa3df17e7f8f5f72069aefa793ea4
AmanAgrahariaa <agraharia329@gmail.com>
2023-10-02 07:27:27 +0530

    modified backend url

--- frontend/src/Components/services.js
+++ frontend/src/Components/services.js
@@ -1,2 +1,3 @@
-export const backend_url = "http://localhost:5000";
-// export const backend_url = 'https://backendattendance-z9uy.onrender.com';
+// export const backend_url = "http://localhost:5000";
+
+export const backend_url = 'https://nssattendancebackend.onrender.com/';
